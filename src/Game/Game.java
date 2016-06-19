6f61ee3609f81421605499c840e798e3f099c115
Saturn91 <manuel.geissberger@gmail.com>
2016-06-19 19:51:25 +0200

    Testing MainStructure

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,5 +1,23 @@
 package Game;
 
+import Main.Main;
+
 public class Game {
+	public Game() {
+		
+	}
+	
+	private int counter = 0;
+	public void tick(){
+		counter++;
+		System.out.println("yep");
+		if(counter >= 10){
+			Main.running = false;
+		}
+	}
+	
+	public void close(){
+		System.out.println("Closing game!");
+	}
 
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,18 @@
 package Main;
 
+import Game.Game;
+
 public class Main {
+	
+	public static boolean running = true;
 
 	public static void main(String[] args) {
+		Game game = new Game();
+		while(running){
+			game.tick();
+		}
 		
+		game.close();
 	}
 
 }
